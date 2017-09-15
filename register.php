a7bff9ef2513a2cee9af7e68dc73452bd3f8dfde
esylva <sylva.etokabeka@juritravail.com>
2017-09-15 13:24:48 +0200

    add register page

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
-<form method="POST" action="index.php?page=signup"  style="text-align:center;">
+<form method="POST" action="index.php?page=signup"  style="text-align:center;" enctype="multipart/form-data">
 <h1>Créer votre profil</h1>
     <div class="form_1" >
-        <div class="form-group">
+        <div class="form-group" style="margin-left:5%;">
             <label for="prenom"></label>
             <input type="text" class="form-control" id="prenom" placeholder="Prénom" name="inscription[prenom]">
         </div>
@@ -9,38 +9,37 @@
             <label for="nom"></label>
             <input type="text" class="form-control" id="nom" placeholder="Nom" name="inscription[nom]">
         </div>
+        <div class="form-group" style="margin-left:5%;">
+            <label for="ville"></label>
+            <input type="text" class="form-control" id="ville" placeholder="Ville" name="inscription[ville]">
+        </div>
     </div>
-    <div class="form-group" style="margin-top:5px;">
+    <div class="form-group" style="margin-top:10px;">
         <select class="form-control" name="inscription[poste]">
             <optgroup label="Administration">
             <option value="9">Directeur général</option>
             <option value="4">Comptable</option>
             <option value="5">Assistant de direction</option>
             <option value="7">Direction administrative</option>
+            <option value="10">Responsable RH</option>
             <optgroup label="Editorial">
             <option value="8">Rédacteur web</option>
             <optgroup label="Gestion / management">
             <option value="6">Chef de produit</option>
             <option value="2">Chef de projet</option>
+            <option value="2">QA</option>
             <optgroup label="Technique">
             <option value="1">Développeur web</option>
             <option value="3">Intégrateur web </option>
-        </select>
-    </div>
-    <div class="form-group">
-        <select class="form-control" id="businessUnit" name="inscription[bu]">
-            <option value="">Business Unit...</option>
-            <option value="Juritravail">Juritravail</option>
-            <option value="Wengo">Habitatpresto</option>
-            <option value="RDVMedicaux">Bordas</option>
-            <option value="RDVMedicaux">RDVMedicaux</option>
-            <option value="Wengo">Wengo</option>
+                <optgroup label="Juridique">
+            <option value="1">Juriste</option>
+            <option value="3">Avocat</option>
         </select>
     </div>
     <div class="form-group" style="margin-top:20px;">
         <img style="margin-right:5px;" src="img/mbp.png" alt="" width="40"/>
         <label for="photo" style="color:#000;font-weight: 500;">Une petite photo ?</label>
-        <input type="file" id="photo" name="inscription[image]" style="display:none;">
+        <input type="file" id="photo" name="image" style="display:none;">
     </div>
     <button type="submit" class="btn btn-default" style="margin-top:5px;">Créer</button>
 </form>
--- signup.php
+++ signup.php
@@ -1,5 +1,12 @@
 <?php
     session_start();
-    $_SESSION = $_POST['inscription'];
-    header('Location: /?page=dashboard');
+   if (isset($_POST['inscription'])) {
+       $_SESSION = $_POST['inscription'];
+       if (isset($_FILES['image'])) {
+           $img = __DIR__ . '/img/';
+           move_uploaded_file($_FILES['image']['tmp_name'], $img . '/'. $_FILES['image']['name']);
+           $_SESSION['image'] = $_FILES['image']['name'];
+       }
+       header('Location: /?page=home');
+   }
     exit;
